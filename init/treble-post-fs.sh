466f234570aad312300aac75e524c81289e3de74
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-02-19 14:54:34 +0000

    gsi: Import PHH copyprop and apply to Xiaomi Civi
    Some new Mi devices only reveal high refresh rate on `ro.product.manufacturer=Xiaomi`
    Until the offending blob is pinpointed, fake the prop with copyprop
    
    Change-Id: I125cc55d6c3d57d7e62c65c997de086cca24abdd

--- init/treble-post-fs.sh
+++ init/treble-post-fs.sh
@@ -4,3 +4,14 @@ mkdir -p /mnt/phh
 mount -t tmpfs -o rw,nodev,relatime,mode=755,gid=0 none /mnt/phh || true
 mkdir /mnt/phh/empty_dir
 touch /mnt/phh/empty_file
+
+copyprop() {
+    p="$(getprop "$2")"
+    if [ "$p" ]; then
+        resetprop_sys "$1" "$(getprop "$2")"
+    fi
+}
+
+if getprop ro.vendor.build.fingerprint | grep -qi -e iaomi/mona; then
+    copyprop ro.product.manufacturer ro.product.vendor.manufacturer
+fi

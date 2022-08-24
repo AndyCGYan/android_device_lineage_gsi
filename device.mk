fbec4d01f3ddce7538bbc682c4579db772083d78
Peter Cai <peter@typeblog.net>
2022-08-24 15:47:06 -0400

    gsi: Initial implementation of sysbta, a system-side bluetooth audio HAL
    On platform release T, the legacy `audio.a2dp.default` HAL no longer
    exists, and cannot be trivially restored for generic system-side A2DP
    audio support. Rather than trying to cling on to its existence (which
    does not even work anymore even if one did so), it is time to come up
    with a new solution.
    
    This commit introduces a system-side implementation of the generic
    bluetooth audio HAL. The HAL itself is modelled after the default
    bluetooth audio HAL, while substantial components of the default audio
    HAL are also included as it has to also implement the audio HAL
    interfaces.
    
    The audio HAL implementation is delegated to `audio.sysbta.default`,
    forked from `audio.bluetooth.default` from the Bluetooth apex package.
    It then communicates with the bluetooth audio HAL interfaces, which need
    to live in the same process (in this case, the process of the sysbta
    HAL). This is why we cannot just load `audio.sysbta.default` into the
    default audio HAL or the audioserver process directly, but rather have
    to include our own simplistic audio HAL implementation.
    
    For now, the audio HAL implementation only includes one for the 6.0
    version, but technically, an interface for *every* audio HAL version
    needs to exist. This, along with other messiness in the sysbta
    implementation, will be addressed in a future commit.
    
    Note that to actually make use of the sysbta audio hal, patches in
    frameworks/av and packages/modules/Bluetooth are required to introduce
    support for the property `persist.bluetooth.system_audio_hal.enabled`.

--- device.mk
+++ device.mk
@@ -1,10 +1,19 @@
 DEVICE_PATH := device/lineage/gsi
 
+# Bluetooth Audio (System-side HAL, sysbta)
+PRODUCT_PACKAGES += \
+    audio.sysbta.default \
+    android.hardware.bluetooth.audio-service-system
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/bluetooth/audio/config/sysbta_audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysbta_audio_policy_configuration.xml \
+    $(DEVICE_PATH)/bluetooth/audio/config/sysbta_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysbta_audio_policy_configuration_7_0.xml
+
 # Fixes inherited from PHH
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/init/init-gsi.rc:system/etc/init/init-gsi.rc \
-    $(DEVICE_PATH)/init/treble-post-fs.sh:system/bin/treble-post-fs.sh \
-    $(DEVICE_PATH)/init/treble-prop-handler.sh:system/bin/treble-prop-handler.sh
+    $(DEVICE_PATH)/init/init-gsi.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init-gsi.rc \
+    $(DEVICE_PATH)/init/treble-post-fs.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/treble-post-fs.sh \
+    $(DEVICE_PATH)/init/treble-prop-handler.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/treble-prop-handler.sh
 
 PRODUCT_PACKAGES += \
     resetprop_sys

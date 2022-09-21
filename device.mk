08fb6193381e7c004d3c1bdbcadf46c3599f8e29
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-09-21 13:53:58 +0000

    gsi: Import PHH & Lineage device overlays

--- device.mk
+++ device.mk
@@ -1,4 +1,9 @@
 DEVICE_PATH := device/lineage/gsi
 
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)

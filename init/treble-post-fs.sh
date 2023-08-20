2ab6c6863a4e2333576f264da49c8440cbcb43dd
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2023-08-20 21:21:05 +0800

    gsi: Only enable meizu-vibrator on Meizu devices
    ...otherwise, on early-init, mount-bind-empty everything related
    Fixes bootloop on devices with no vibrator, e.g. tablets
    
    Change-Id: I3efad162075bcf6bb487915b4a23a5f25449f6b7

--- init/treble-post-fs.sh
+++ init/treble-post-fs.sh
@@ -1,10 +1,5 @@
 #!/system/bin/sh
 
-mkdir -p /mnt/phh
-mount -t tmpfs -o rw,nodev,relatime,mode=755,gid=0 none /mnt/phh || true
-mkdir /mnt/phh/empty_dir
-touch /mnt/phh/empty_file
-
 copyprop() {
     p="$(getprop "$2")"
     if [ "$p" ]; then
